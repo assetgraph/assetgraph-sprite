a7db1851620eac2b39db9c70540226b792b4ab37
Andreas Lind <andreaslindpetersen@gmail.com>
2018-01-28 18:21:37 +0100

    Replace jshint with eslint

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+    extends: [
+        'onelint'
+    ],
+    rules: {
+        'no-const-assign': 2
+    },
+    env: {
+        es6: true
+    },
+    parserOptions: {
+        ecmaVersion: 2017,
+        ecmaFeatures: {
+            experimentalObjectRestSpread: true
+
+        }
+    }
+};
